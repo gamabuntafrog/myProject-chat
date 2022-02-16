595b45c97bbf30b7573bd18178206fa63754b152
gamabuntafrog <kirik.slaw@gmail.com>
2022-02-16 12:06:18 +0200

    Спрятял конфиг

--- src/index.js
+++ src/index.js
@@ -16,13 +16,7 @@ import { createTheme, ThemeProvider,CssBaseline } from '@mui/material';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBvNxoUQWQehZKeaU_uaQ6Rxq4BFpODRxc",
-  authDomain: "gamabunta-chat.firebaseapp.com",
-  projectId: "gamabunta-chat",
-  storageBucket: "gamabunta-chat.appspot.com",
-  messagingSenderId: "348555320114",
-  appId: "1:348555320114:web:c877b2222d8213bcc7d52b",
-  measurementId: "G-3832YNV6K3"
+
 };
 
 export const Context = createContext(null)
